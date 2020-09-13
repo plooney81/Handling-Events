3f91e4a52c46333c8fdf2389bb8e2cc801177365
Pete Looney <petelooney81@gmail.com>
2020-09-13 13:33:54 -0500

    button works

--- medium-ex.js
+++ medium-ex.js
@@ -1,38 +1,46 @@
-function renderButton(button){
-    return `
-        <div>
-            <button type="button" id="${button}">${button}</button>
-        </div>
-    `
-}
+// function renderButton(button){
+//     return `
+//         <div>
+//             <button type="button" id="${button}">${button}</button>
+//         </div>
+//     `
+// }
 
 function newButton(buttonName) {
-    console.log('Yo')
     let content = document.querySelector('.content');
+
+    newStuff = `<button type="button" id="${buttonName}">${buttonName}</button>`
     
-
-    content.innerHTML += renderButton(buttonName);
+    content.innerHTML += '<div>' + newStuff + '</div>';
 }
 
 
-function renderInput(){
+// function renderInput(){
 
-    return `
-        <form>
-            <input type="text">
-        </form>
-    `
-}
+//     return `
+//         <form>
+//             <input type="text">
+//         </form>
+//     `
+// }
 
 function inputForm(){
     let content = document.querySelector('.content');
 
-    content.innerHTML += renderInput();
+    let newStuff = `
+        <form>
+            <input type="text">
+        </form>
+    `
+
+
+    content.innerHTML += '<div>' + newStuff + '</div>';
 }
 
 function clearInput(){
     document.querySelector('input').value = '';
 }
+
 
 // function addToList(){
 //     let buttonName = document.querySelector('input').value;
@@ -43,10 +51,23 @@ function clearInput(){
 // }
 
 inputForm();
-newButton('Add');
 
-const addBut = document.querySelector('#Add');
-addBut.addEventListener('click', ()=>{
-    newButton(document.querySelector('input').value);
-    clearInput();
-});
+// newButton('Pete');
+// newButton('David');
+// newButton('Shannon');
+// newButton('Beth');
+
+// const addBut = document.querySelector('#add');
+// addBut.addEventListener('click', ()=>{
+//     newButton(document.querySelector('input').value);
+//     clearInput();
+// });
+
+
+// addBut.addEventListener('click', alertFunct);
+
+// window.addEventListener('DOMContentLoaded', function(){
+//     const addBut = document.querySelector('button');
+//     addBut.addEventListener('click', newButton(document.querySelector('input').value));
+// });
+
